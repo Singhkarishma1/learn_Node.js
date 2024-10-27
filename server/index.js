d1b20c51a7ebed298b2fbc60e9c8293a4e1bb3ec
Singhkarishma1 <145593589+Singhkarishma1@users.noreply.github.com>
2024-10-28 00:10:09 +0530

    server handling

--- server/index.js
+++ server/index.js
@@ -1,15 +1,47 @@
 const http = require("http");
 
 const fs = require("fs");
+const url=require("url");
 
 const myServer = http.createServer((req,res)=>{
     // console.log("New Req Rec.");
     // console.log(req);
-    const log=`${Date.now()}:New req received\n`;
+    if(req.url=="/favicon.ico")return res.end();
+    const log=`${Date.now()}:${req.method} ${req.url}New req received\n`;
+    // fs.appendFile("log.txt",log,(err,data)=>{
+    // res.end("Hello from server ");
+    const myUrl=url.parse(req.url,true);
     fs.appendFile("log.txt",log,(err,data)=>{
-    res.end("Hello from server ");
+switch(myUrl.pathname){
+    case "/" :   
+            res.end("HomePage");
+            break;
+            case "/about":
+                const username=myUrl.query.myname;
+                res.end(`hi,${username}`);
+           
+                break;
+            case "/search":
+               
+                    const search=myUrl.query.search_query;
+                    res.end(`search result for ${search}`);
+                
+                break;
+                case "/signup":
+                    if(req.method=="GET")res.end("signup page");
+                    else if(req.method=="POST")
+                        {
+                            res.end("success");
+               
+                        }
+                         default:
+                    res.end("404 page not found");
+                    break;
+                
+                }
+    });
    });
-});
+
 
 
 myServer.listen(3000,()=>console.log("Server is running on port 8000")); //port number should be greater than 3000  
